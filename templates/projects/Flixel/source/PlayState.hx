2c88c27968067b55bdd6cf47eb664bad7b5b3d0e
Jarrio <jarrio@users.noreply.github.com>
2017-05-25 04:33:10 +0100

    Updated playstate tpl to include more imports

--- templates/projects/Flixel/source/PlayState.hx
+++ templates/projects/Flixel/source/PlayState.hx
@@ -1,6 +1,11 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.ui.FlxButton;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState {
     override public function create():Void {
